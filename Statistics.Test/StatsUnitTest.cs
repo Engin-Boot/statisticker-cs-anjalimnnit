1a65fbfb68f5240350500702bfb8491d6a247c09
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-09-02 22:24:09 -0700

    updated tests

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,27 +1,36 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Statistics.Test
 {
     public class StatsUnitTest
     {
+        
         [Fact]
         public void ReportsAverageMinMax()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+                new List<double>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Min - 1.5) <= epsilon);
         }
+        [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<double>{});
+            Assert.True(double.IsNaN(computedStats.Average));
+            Assert.True(double.IsNaN(computedStats.Max));
+            Assert.True(double.IsNaN(computedStats.Min));
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿  
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Statistics
 {
+    
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public Stats CalculateStatistics(List<double> numbers)
+        {
+            Stats obj = new Stats();
+            if (numbers.Count == 0) {
+                obj.Max = double.NaN;
+                obj.Min = double.NaN;
+                obj.Average = double.NaN;
+                return obj;
+            }
+            obj.Max = numbers.Max();
+            obj.Min = numbers.Min();
+            obj.Average = numbers.Average();
+            return obj;
         }
     }
 }
